d7935f1aaf88186c0464b9e21b2d26e8b387f517
Vighnesh-code <vighneshzagade4@gmail.com>
2025-05-23 04:13:47 +0000

    initialized react projects and created Navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import Logo from "../assets/logo.png";
+import { MdMenu } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
+const NavbarMenu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Categories",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "About",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    link: "#",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className="text-white py-8 bg-orange-500">
+      <div className="container flex justify-between items-center">
+        {/* logo section */}
+        <div>
+          <img src={Logo} alt="logo" className="max-w-[100px] invert" />
+        </div>
+
+        {/* Menu Section */}
+        <div className="hidden md:block">
+          <ul className="flex items-center gap-4 relative z-40">
+            {NavbarMenu.map((item) => (
+              <li>
+                <a
+                  href={item.link}
+                  className="inline-block text-base font-semibold py-2 px-3 uppercase"
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+            <button className="text-xl ps-14">
+              <FaRegUser />
+            </button>
+          </ul>
+        </div>
+
+        {/* Hamburger Icon */}
+        <div className="md:hidden">
+          <MdMenu className="text-4xl" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
